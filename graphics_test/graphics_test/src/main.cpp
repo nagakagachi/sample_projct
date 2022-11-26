fb32f2d86fde10acea04d03c84153c3c3f811d3d
nagakagachi <nagakagachi@gmail.com>
2022-11-26 23:20:53 +0900

    math matrix関連機能追加
    Mat22 Mat33 Mat44の基本的な演算実装.

--- graphics_test/graphics_test/src/main.cpp
+++ graphics_test/graphics_test/src/main.cpp
@@ -155,6 +155,35 @@ AppGame::AppGame()
 {
 	ngl::math::funcAA();
 
+	const ngl::math::Mat44 k_inv_test_00 = ngl::math::Mat44::Identity();
+
+	const ngl::math::Mat44 k_rot_z = ngl::math::Mat44(std::cos(0.5f), -std::sin(0.5f), 0, 0, /**/ std::sin(0.5f), std::cos(0.5f), 0, 0, /**/ 0, 0, 1, 0, /**/ 0, 0, 0, 1);
+	const ngl::math::Mat44 k_rot_z_inv = ngl::math::Mat44::inverse(k_rot_z);
+
+
+	const ngl::math::Mat44 k_rot_x = ngl::math::Mat44(1, 0, 0, 0, /**/ 0, std::cos(0.5f), -std::sin(0.5f), 0, /**/ 0, std::sin(0.5f), std::cos(0.5f), 0, /**/ 0, 0, 0, 1);
+	const ngl::math::Mat44 k_rot_x_inv = ngl::math::Mat44::inverse(k_rot_x);
+
+	const ngl::math::Mat44 k_rot_y = ngl::math::Mat44(std::cos(0.5f), 0, -std::sin(0.5f), 0, /**/ 0, 1, 0, 0, /**/ std::sin(0.5f), 0, std::cos(0.5f), 0, /**/ 0, 0, 0, 1);
+	const ngl::math::Mat44 k_rot_y_inv = ngl::math::Mat44::inverse(k_rot_y);
+
+
+	const auto m_0 = k_rot_z * k_rot_x * ngl::math::Mat44::inverse(k_rot_z * k_rot_x);
+	const auto m_1 = k_rot_x * k_rot_x_inv;
+	const auto m_2 = k_rot_y * k_rot_y_inv;
+
+
+
+	const ngl::math::Mat33 k_rot33_z = ngl::math::Mat33(std::cos(0.5f), -std::sin(0.5f), 0, /**/ std::sin(0.5f), std::cos(0.5f), 0, /**/ 0, 0, 1);
+	const ngl::math::Mat33 k_rot33_z_inv = ngl::math::Mat33::inverse(k_rot33_z);
+	const auto k_rot33_z_mul = k_rot33_z * k_rot33_z_inv;
+
+
+	const ngl::math::Mat22 k_rot22_z = ngl::math::Mat22(std::cos(0.5f), -std::sin(0.5f),/**/ std::sin(0.5f), std::cos(0.5f));
+	const ngl::math::Mat22 k_rot22_z_inv = ngl::math::Mat22::inverse(k_rot22_z);
+	const auto k_rot22_z_mul = k_rot22_z * k_rot22_z_inv;
+
+
 }
 AppGame::~AppGame()
 {
--- graphics_test/graphics_test/src/ngl/math/math.h
+++ graphics_test/graphics_test/src/ngl/math/math.h
@@ -30,20 +30,20 @@ namespace ngl
 			VecComponentT() = default;
 			constexpr VecComponentT(const VecComponentT& v) = default;
 			explicit constexpr VecComponentT(float v)
-				: x(v) , y(v)
+				: x(v), y(v)
 			{}
 			constexpr VecComponentT(float _x, float _y, float _dummy0, float _dummy1)
-				: x(_x) , y(_y)
+				: x(_x), y(_y)
 			{}
 			// 要素修正付きコンストラクタ.
 			template<typename ComponentModifyFunc>
 			constexpr VecComponentT(float _x, float _y, float _dummy0, float _dummy1, ComponentModifyFunc func)
-				: x(func(_x)) , y(func(_y))
+				: x(func(_x)), y(func(_y))
 			{}
 			// 要素修正付きコンストラクタ.
 			template<typename ComponentModifyFunc>
 			constexpr VecComponentT(const float* _data, ComponentModifyFunc func)
-				: x(func(_data[0])) , y(func(_data[1]))
+				: x(func(_data[0])), y(func(_data[1]))
 			{}
 		};
 
@@ -64,20 +64,20 @@ namespace ngl
 			VecComponentT() = default;
 			constexpr VecComponentT(const VecComponentT& v) = default;
 			explicit constexpr VecComponentT(float v)
-				: x(v) , y(v) , z(v)
+				: x(v), y(v), z(v)
 			{}
 			constexpr VecComponentT(float _x, float _y, float _z, float _dummy0)
-				: x(_x) , y(_y) , z(_z)
+				: x(_x), y(_y), z(_z)
 			{}
 			// 要素修正付きコンストラクタ.
 			template<typename ComponentModifyFunc>
 			constexpr VecComponentT(float _x, float _y, float _z, float _dummy0, ComponentModifyFunc func)
-				: x(func(_x)) , y(func(_y)), z(func(_z))
+				: x(func(_x)), y(func(_y)), z(func(_z))
 			{}
 			// 要素修正付きコンストラクタ.
 			template<typename ComponentModifyFunc>
 			constexpr VecComponentT(const float* _data, ComponentModifyFunc func)
-				: x(func(_data[0])) , y(func(_data[1])) , z(func(_data[2]))
+				: x(func(_data[0])), y(func(_data[1])), z(func(_data[2]))
 			{}
 		};
 
@@ -98,20 +98,20 @@ namespace ngl
 			VecComponentT() = default;
 			constexpr VecComponentT(const VecComponentT& v) = default;
 			explicit constexpr VecComponentT(float v)
-				: x(v) , y(v) , z(v) , w(v)
+				: x(v), y(v), z(v), w(v)
 			{}
 			constexpr VecComponentT(float _x, float _y, float _z, float _w)
-				: x(_x) , y(_y) , z(_z) , w(_w)
+				: x(_x), y(_y), z(_z), w(_w)
 			{}
 			// 要素修正付きコンストラクタ.
 			template<typename ComponentModifyFunc>
 			constexpr VecComponentT(float _x, float _y, float _z, float _w, ComponentModifyFunc func)
-				: x(func(_x)) , y(func(_y)) , z(func(_z)) , w(func(_w))
+				: x(func(_x)), y(func(_y)), z(func(_z)), w(func(_w))
 			{}
 			// 要素修正付きコンストラクタ.
 			template<typename ComponentModifyFunc>
 			constexpr VecComponentT(const float* _data, ComponentModifyFunc func)
-				: x(func(_data[0])) , y(func(_data[1])) , z(func(_data[2])) , w(func(_data[3]))
+				: x(func(_data[0])), y(func(_data[1])), z(func(_data[2])), w(func(_data[3]))
 			{}
 		};
 
@@ -152,12 +152,12 @@ namespace ngl
 				: VecComponentT<N>(_data, func)
 			{
 			}
-			
+
 
 			// += operator Broadcast.
 			VecN& operator +=(const VecN& v)
 			{
-				for(auto i = 0; i < N; ++i)
+				for (auto i = 0; i < N; ++i)
 					this->data[i] += v.data[i];
 				return *this;
 			}
@@ -217,7 +217,7 @@ namespace ngl
 				{
 					return VecN(
 						v0.y * v1.z - v1.y * v0.z,
-						v0.z * v1.x - v1.z * v0.x, 
+						v0.z * v1.x - v1.z * v0.x,
 						v0.x * v1.y - v1.x * v0.y);
 				}
 			}
@@ -251,7 +251,7 @@ namespace ngl
 				return VecN(0.0f, 0.0f, 0.0f, 1.0f);
 			}
 		};
-		
+
 		using Vec2 = VecN<2>;
 		using Vec3 = VecN<3>;
 		using Vec4 = VecN<4>;
@@ -398,8 +398,8 @@ namespace ngl
 
 
 
-			ngl::math::Vec3 v3_4(1,2,3);
-			v3_4 /= ngl::math::Vec3(3,2,1) / ngl::math::Vec3(2);
+			ngl::math::Vec3 v3_4(1, 2, 3);
+			v3_4 /= ngl::math::Vec3(3, 2, 1) / ngl::math::Vec3(2);
 
 			constexpr auto v3_5 = ngl::math::Vec3(2) / ngl::math::Vec3(4);
 			constexpr auto v3_6 = ngl::math::Vec3(2) * ngl::math::Vec3(4);
@@ -421,6 +421,266 @@ namespace ngl
 		}
 
 
+
+		// -----------------------------------------------------------------------------------------
+		struct Mat22
+		{
+			union
+			{
+				float	m[2][2];
+				struct
+				{
+					Vec2	r0, r1;
+				};
+			};
+
+			Mat22() = default;
+			constexpr Mat22(const Mat22& v) = default;
+			explicit constexpr Mat22(float v)
+				: r0(v), r1(v)
+			{
+			}
+			constexpr Mat22(
+				float _m00, float _m01,
+				float _m10, float _m11)
+				: r0(_m00, _m01)
+				, r1(_m10, _m11)
+			{
+			}
+			constexpr Mat22(const Vec2& row0, const Vec2& row1)
+				: r0(row0)
+				, r1(row1)
+			{
+			}
+
+			static constexpr Mat22 Zero()
+			{
+				return Mat22(0.0f);
+			}
+			static constexpr Mat22 Identity()
+			{
+				return {
+					Vec2::UnitX(),
+					Vec2::UnitY(),
+				};
+			}
+
+			// 転置
+			static constexpr Mat22 transpose(const Mat22& m)
+			{
+				return Mat22(
+					m.r0.x, m.r1.x,
+					m.r0.y, m.r1.y
+				);
+			}
+			// 行列式.
+			static constexpr float determinant(const Mat22& m)
+			{
+				return m.r0.x * m.r1.y - m.r0.y * m.r1.x;
+			}
+			// 逆行列.
+			static constexpr Mat22 inverse(const Mat22& m)
+			{
+				const float c00 = m.r1.y;
+				const float c01 = -m.r1.x;
+
+				const float c10 = -m.r0.y;
+				const float c11 = m.r0.x;
+
+				const float det = m.r0.x * c00 + m.r0.y * c01;
+				const float inv_det = 1.0f / det;
+
+				return Mat22(
+					c00 * inv_det, c10 * inv_det,
+					c01 * inv_det, c11 * inv_det
+				);
+			}
+		};
+		// Mat + Mat
+		inline constexpr Mat22 operator+(const Mat22& m0, const Mat22& m1)
+		{
+			return Mat22(
+				m0.r0 + m1.r0,
+				m0.r1 + m1.r1
+			);
+		}
+		// Mat - Mat
+		inline constexpr Mat22 operator-(const Mat22& m0, const Mat22& m1)
+		{
+			return Mat22(
+				m0.r0 - m1.r0,
+				m0.r1 - m1.r1
+			);
+		}
+		// Mat * Mat
+		inline constexpr Mat22 operator*(const Mat22& m0, const Mat22& m1)
+		{
+			return Mat22(
+				m0.r0.x * m1.r0 + m0.r0.y * m1.r1,
+				m0.r1.x * m1.r0 + m0.r1.y * m1.r1
+			);
+		}
+		// Mat * scalar
+		inline constexpr Mat22 operator*(const Mat22& m0, float s)
+		{
+			return Mat22(
+				m0.r0 * s,
+				m0.r1 * s
+			);
+		}
+		// scalar * Mat
+		inline constexpr Mat22 operator*(float s, const Mat22& m0)
+		{
+			return m0 * s;
+		}
+		// Mat / scalar
+		inline constexpr Mat22 operator/(const Mat22& m0, float s)
+		{
+			const auto s_inv = 1.0f / s;
+			return m0 * s_inv;
+		}
+		// -----------------------------------------------------------------------------------------
+
+		// -----------------------------------------------------------------------------------------
+		struct Mat33
+		{
+			union
+			{
+				float	m[3][3];
+				struct
+				{
+					Vec3	r0, r1, r2;
+				};
+			};
+
+			Mat33() = default;
+			constexpr Mat33(const Mat33& v) = default;
+			explicit constexpr Mat33(float v)
+				: r0(v), r1(v), r2(v)
+			{
+			}
+			constexpr Mat33(
+				float _m00, float _m01, float _m02,
+				float _m10, float _m11, float _m12,
+				float _m20, float _m21, float _m22)
+				: r0(_m00, _m01, _m02)
+				, r1(_m10, _m11, _m12)
+				, r2(_m20, _m21, _m22)
+			{
+			}
+			constexpr Mat33(const Vec3& row0, const Vec3& row1, const Vec3& row2)
+				: r0(row0)
+				, r1(row1)
+				, r2(row2)
+			{
+			}
+
+			static constexpr Mat33 Zero()
+			{
+				return Mat33(0.0f);
+			}
+			static constexpr Mat33 Identity()
+			{
+				return {
+					Vec3::UnitX(),
+					Vec3::UnitY(),
+					Vec3::UnitZ()
+				};
+			}
+
+			// 転置
+			static constexpr Mat33 transpose(const Mat33& m)
+			{
+				return Mat33(
+					m.r0.x, m.r1.x, m.r2.x,
+					m.r0.y, m.r1.y, m.r2.y,
+					m.r0.z, m.r1.z, m.r2.z
+				);
+			}
+			// 行列式.
+			static constexpr float determinant(const Mat33& m)
+			{
+				const float c00 = Mat22::determinant(Mat22(m.r1.y, m.r1.z, m.r2.y, m.r2.z));
+				const float c01 = -Mat22::determinant(Mat22(m.r1.x, m.r1.z, m.r2.x, m.r2.z));
+				const float c02 = Mat22::determinant(Mat22(m.r1.x, m.r1.y, m.r2.x, m.r2.y));
+				return m.r0.x * c00 + m.r0.y * c01 + m.r0.z * c02;
+			}
+			// 逆行列.
+			static constexpr Mat33 inverse(const Mat33& m)
+			{
+				const float c00 = Mat22::determinant(Mat22(m.r1.y, m.r1.z, m.r2.y, m.r2.z));
+				const float c01 = -Mat22::determinant(Mat22(m.r1.x, m.r1.z, m.r2.x, m.r2.z));
+				const float c02 = Mat22::determinant(Mat22(m.r1.x, m.r1.y, m.r2.x, m.r2.y));
+
+				const float c10 = -Mat22::determinant(Mat22(m.r0.y, m.r0.z, m.r2.y, m.r2.z));
+				const float c11 = Mat22::determinant(Mat22(m.r0.x, m.r0.z, m.r2.x, m.r2.z));
+				const float c12 = -Mat22::determinant(Mat22(m.r0.x, m.r0.y, m.r2.x, m.r2.y));
+
+				const float c20 = Mat22::determinant(Mat22(m.r0.y, m.r0.z, m.r1.y, m.r1.z));
+				const float c21 = -Mat22::determinant(Mat22(m.r0.x, m.r0.z, m.r1.x, m.r1.z));
+				const float c22 = Mat22::determinant(Mat22(m.r0.x, m.r0.y, m.r1.x, m.r1.y));
+
+				const float det = m.r0.x * c00 + m.r0.y * c01 + m.r0.z * c02;
+				const float inv_det = 1.0f / det;
+
+				return Mat33(
+					c00 * inv_det, c10 * inv_det, c20 * inv_det,
+					c01 * inv_det, c11 * inv_det, c21 * inv_det,
+					c02 * inv_det, c12 * inv_det, c22 * inv_det
+				);
+			}
+		};
+		// Mat + Mat
+		inline constexpr Mat33 operator+(const Mat33& m0, const Mat33& m1)
+		{
+			return Mat33(
+				m0.r0 + m1.r0,
+				m0.r1 + m1.r1,
+				m0.r2 + m1.r2
+			);
+		}
+		// Mat - Mat
+		inline constexpr Mat33 operator-(const Mat33& m0, const Mat33& m1)
+		{
+			return Mat33(
+				m0.r0 - m1.r0,
+				m0.r1 - m1.r1,
+				m0.r2 - m1.r2
+			);
+		}
+		// Mat * Mat
+		inline constexpr Mat33 operator*(const Mat33& m0, const Mat33& m1)
+		{
+			return Mat33(
+				m0.r0.x * m1.r0 + m0.r0.y * m1.r1 + m0.r0.z * m1.r2,
+				m0.r1.x * m1.r0 + m0.r1.y * m1.r1 + m0.r1.z * m1.r2,
+				m0.r2.x * m1.r0 + m0.r2.y * m1.r1 + m0.r2.z * m1.r2
+			);
+		}
+		// Mat * scalar
+		inline constexpr Mat33 operator*(const Mat33& m0, float s)
+		{
+			return Mat33(
+				m0.r0 * s,
+				m0.r1 * s,
+				m0.r2 * s
+			);
+		}
+		// scalar * Mat
+		inline constexpr Mat33 operator*(float s, const Mat33& m0)
+		{
+			return m0 * s;
+		}
+		// Mat / scalar
+		inline constexpr Mat33 operator/(const Mat33& m0, float s)
+		{
+			const auto s_inv = 1.0f / s;
+			return m0 * s_inv;
+		}
+		// -----------------------------------------------------------------------------------------
+
+
+		// -----------------------------------------------------------------------------------------
 		struct Mat44
 		{
 			union
@@ -478,7 +738,116 @@ namespace ngl
 					Vec4::UnitW(),
 				};
 			}
+
+			// 転置
+			static constexpr Mat44 transpose(const Mat44& m)
+			{
+				return Mat44(
+					m.r0.x, m.r1.x, m.r2.x, m.r3.x,
+					m.r0.y, m.r1.y, m.r2.y, m.r3.y,
+					m.r0.z, m.r1.z, m.r2.z, m.r3.z,
+					m.r0.w, m.r1.w, m.r2.w, m.r3.w
+				);
+			}
+
+			// 行列式.
+			static constexpr float determinant(const Mat44& m)
+			{
+				const float c00 = Mat33::determinant(Mat33(m.r1.y, m.r1.z, m.r1.w, m.r2.y, m.r2.z, m.r2.w, m.r3.y, m.r3.z, m.r3.w));
+				const float c01 = -Mat33::determinant(Mat33(m.r1.x, m.r1.z, m.r1.w, m.r2.x, m.r2.z, m.r2.w, m.r3.x, m.r3.z, m.r3.w));
+				const float c02 = Mat33::determinant(Mat33(m.r1.x, m.r1.y, m.r1.w, m.r2.x, m.r2.y, m.r2.w, m.r3.x, m.r3.y, m.r3.w));
+				const float c03 = -Mat33::determinant(Mat33(m.r1.x, m.r1.y, m.r1.z, m.r2.x, m.r2.y, m.r2.z, m.r3.x, m.r3.y, m.r3.z));
+				const float det = m.r0.x * c00 + m.r0.y * c01 + m.r0.z * c02 + m.r0.w * c03;
+				return det;
+			}
+			// 逆行列.
+			static constexpr Mat44 inverse(const Mat44& m)
+			{
+				const float c00 = Mat33::determinant(Mat33(m.r1.y, m.r1.z, m.r1.w, m.r2.y, m.r2.z, m.r2.w, m.r3.y, m.r3.z, m.r3.w));
+				const float c01 = -Mat33::determinant(Mat33(m.r1.x, m.r1.z, m.r1.w, m.r2.x, m.r2.z, m.r2.w, m.r3.x, m.r3.z, m.r3.w));
+				const float c02 = Mat33::determinant(Mat33(m.r1.x, m.r1.y, m.r1.w, m.r2.x, m.r2.y, m.r2.w, m.r3.x, m.r3.y, m.r3.w));
+				const float c03 = -Mat33::determinant(Mat33(m.r1.x, m.r1.y, m.r1.z, m.r2.x, m.r2.y, m.r2.z, m.r3.x, m.r3.y, m.r3.z));
+
+				const float c10 = -Mat33::determinant(Mat33(m.r0.y, m.r0.z, m.r0.w, m.r2.y, m.r2.z, m.r2.w, m.r3.y, m.r3.z, m.r3.w));
+				const float c11 = Mat33::determinant(Mat33(m.r0.x, m.r0.z, m.r0.w, m.r2.x, m.r2.z, m.r2.w, m.r3.x, m.r3.z, m.r3.w));
+				const float c12 = -Mat33::determinant(Mat33(m.r0.x, m.r0.y, m.r0.w, m.r2.x, m.r2.y, m.r2.w, m.r3.x, m.r3.y, m.r3.w));
+				const float c13 = Mat33::determinant(Mat33(m.r0.x, m.r0.y, m.r0.z, m.r2.x, m.r2.y, m.r2.z, m.r3.x, m.r3.y, m.r3.z));
+
+				const float c20 = Mat33::determinant(Mat33(m.r0.y, m.r0.z, m.r0.w, m.r1.y, m.r1.z, m.r1.w, m.r3.y, m.r3.z, m.r3.w));
+				const float c21 = -Mat33::determinant(Mat33(m.r0.x, m.r0.z, m.r0.w, m.r1.x, m.r1.z, m.r1.w, m.r3.x, m.r3.z, m.r3.w));
+				const float c22 = Mat33::determinant(Mat33(m.r0.x, m.r0.y, m.r0.w, m.r1.x, m.r1.y, m.r1.w, m.r3.x, m.r3.y, m.r3.w));
+				const float c23 = -Mat33::determinant(Mat33(m.r0.x, m.r0.y, m.r0.z, m.r1.x, m.r1.y, m.r1.z, m.r3.x, m.r3.y, m.r3.z));
+
+				const float c30 = -Mat33::determinant(Mat33(m.r0.y, m.r0.z, m.r0.w, m.r1.y, m.r1.z, m.r1.w, m.r2.y, m.r2.z, m.r2.w));
+				const float c31 = Mat33::determinant(Mat33(m.r0.x, m.r0.z, m.r0.w, m.r1.x, m.r1.z, m.r1.w, m.r2.x, m.r2.z, m.r2.w));
+				const float c32 = -Mat33::determinant(Mat33(m.r0.x, m.r0.y, m.r0.w, m.r1.x, m.r1.y, m.r1.w, m.r2.x, m.r2.y, m.r2.w));
+				const float c33 = Mat33::determinant(Mat33(m.r0.x, m.r0.y, m.r0.z, m.r1.x, m.r1.y, m.r1.z, m.r2.x, m.r2.y, m.r2.z));
+
+				const float det = m.r0.x * c00 + m.r0.y * c01 + m.r0.z * c02 + m.r0.w * c03;
+				const float inv_det = 1.0f / det;
+
+				return Mat44(
+					c00 * inv_det, c10 * inv_det, c20 * inv_det, c30 * inv_det,
+					c01 * inv_det, c11 * inv_det, c21 * inv_det, c31 * inv_det,
+					c02 * inv_det, c12 * inv_det, c22 * inv_det, c32 * inv_det,
+					c03 * inv_det, c13 * inv_det, c23 * inv_det, c33 * inv_det
+				);
+			}
 		};
+
+		// Mat + Mat
+		inline constexpr Mat44 operator+(const Mat44& m0, const Mat44& m1)
+		{
+			return Mat44(
+				m0.r0 + m1.r0,
+				m0.r1 + m1.r1,
+				m0.r2 + m1.r2,
+				m0.r3 + m1.r3
+			);
+		}
+		// Mat - Mat
+		inline constexpr Mat44 operator-(const Mat44& m0, const Mat44& m1)
+		{
+			return Mat44(
+				m0.r0 - m1.r0,
+				m0.r1 - m1.r1,
+				m0.r2 - m1.r2,
+				m0.r3 - m1.r3
+			);
+		}
+		// Mat * Mat
+		inline constexpr Mat44 operator*(const Mat44& m0, const Mat44& m1)
+		{
+			return Mat44(
+				m0.r0.x * m1.r0 + m0.r0.y * m1.r1 + m0.r0.z * m1.r2 + m0.r0.w * m1.r3,
+				m0.r1.x * m1.r0 + m0.r1.y * m1.r1 + m0.r1.z * m1.r2 + m0.r1.w * m1.r3,
+				m0.r2.x * m1.r0 + m0.r2.y * m1.r1 + m0.r2.z * m1.r2 + m0.r2.w * m1.r3,
+				m0.r3.x * m1.r0 + m0.r3.y * m1.r1 + m0.r3.z * m1.r2 + m0.r3.w * m1.r3
+			);
+		}
+		// Mat * scalar
+		inline constexpr Mat44 operator*(const Mat44& m0, float s)
+		{
+			return Mat44(
+				m0.r0 * s,
+				m0.r1 * s,
+				m0.r2 * s,
+				m0.r3 * s
+			);
+		}
+		// scalar * Mat
+		inline constexpr Mat44 operator*(float s, const Mat44& m0)
+		{
+			return m0 * s;
+		}
+		// Mat / scalar
+		inline constexpr Mat44 operator/(const Mat44& m0, float s)
+		{
+			const auto s_inv = 1.0f / s;
+			return m0 * s_inv;
+		}
+		// -----------------------------------------------------------------------------------------
+
 
 		// row-major Transform Matrix34 が必要な場合の Mat44->Mat34 コンバート用.
 		struct Mat34
